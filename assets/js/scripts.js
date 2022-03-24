b13c6c73908bb6c51c86c035450d994f43cf4d81
João Maia <15joaomaia@gmail.com>
2022-03-24 11:47:22 -0300

    projeto finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,6 @@
-let burger = document.querySelector('.navbar-burger')
+let burger = document.querySelector('.burger')
 let menu = document.querySelector('.navbar-menu')
-let navStart = document.querySelector('.navbar-start')
-
 
 burger.addEventListener('click', (e)=>{
     menu.classList.toggle('is-active')
-    // navStart.classList.toggle('padding-left')
 })
