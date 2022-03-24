2d35ea78692bd023100e88441a529c40f7b7feec
João Maia <15joaomaia@gmail.com>
2022-03-24 17:38:05 -0300

    adicionado o button close no responsivo. projeto finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,23 @@
 let burger = document.querySelector('.burger')
 let menu = document.querySelector('.navbar-menu')
+let closeButton = document.querySelector('.close')
 
 burger.addEventListener('click', (e)=>{
     menu.classList.toggle('is-active')
+    // console.log(menu.classList)
+
+    if(menu.classList == 'navbar-menu is-active'){
+        burger.classList.toggle('is-hidden')
+        closeButton.classList.toggle('is-hidden')
+    }   
+})
+
+closeButton.addEventListener('click', (e)=>{
+    menu.classList.toggle('is-active')
+    // console.log(closeButton.classList)
+
+    if(menu.classList == 'navbar-menu'){
+        burger.classList.toggle('is-hidden')
+        closeButton.classList.toggle('is-hidden')
+    }   
 })
